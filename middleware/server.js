82cd589a68b10204e1ee03d7f57a96f5b59d4f6a
whatpull <whatpull@gmail.com>
2019-11-20 23:10:22 +0900

    노드 서버 코드 정리

--- middleware/server.js
+++ middleware/server.js
@@ -1,6 +1,4 @@
 const http = require("http");
-const fs = require("fs");
-const url = require("url");
 const router = require("./router");
 
 // [기본] 서버 생성
@@ -17,11 +15,3 @@ const server = http.createServer((request, response) => {
 server.listen(80, () => {
     console.log("start server 80");
 });
-
-// 명령어를 통해 Node를 실행하는 셋팅을 진행합니다.
-// 1. 그래프 테스트(GoJS)를 진행합니다.
-// [무료버전이 아니기때문에, 직접 캔버스 제어 패키지 모듈 제작]
-// 2. 데이터 파싱(획득한 데이터를 파싱합니다.)
-// 3. 그래프 정리 및 모양 확인
-// 모듈 파일을 읽기 위해서는 node_modules의 package.json의 main을 읽어 들여 최종 경로는 셋업합니다.
-// 읽어드린 모듈 및 파일을 URL 접근 가능하도록 맵핑합니다.
